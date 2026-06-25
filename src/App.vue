a2b93c756f6716cbcc5f5a0ed1535aca992f8d26
justineninocubelo-wq <justineninocubelo@gmail.com>
2026-06-25 12:04:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import SampleApp from './components/SampleApp.vue'
+
+</script>
+
+<template>
+  <SampleApp />
+</template>
---
+++ src/components/SampleApp.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>My Sample App</h1>
+    <p>Welcome to my sample app!</p>
+  </div>
+</template>
